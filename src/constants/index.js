7b8ae65a21b5731567e829ccb46345d4172234ac
bs-ashish-kumar <ashish.kumar@bombaysoftwares.com>
2024-08-06 01:06:30 +0530

    feature: work experience section [wip]

--- src/constants/index.js
+++ src/constants/index.js
@@ -45,18 +45,15 @@ const services = [
     title: "Web Developer",
     icon: web,
   },
-  {
-    title: "React Native Developer",
-    icon: mobile,
-  },
+  // {
+  //   title: "React Native Developer",
+  //   icon: mobile,
+  // },
   {
     title: "Backend Developer",
     icon: backend,
   },
-  {
-    title: "Content Creator",
-    icon: creator,
-  },
+ 
 ];
 
 const technologies = [
