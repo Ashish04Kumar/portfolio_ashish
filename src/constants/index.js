763d0448ef5d496d0472dd52a61663a74bfcd4b4
bs-ashish-kumar <ashish.kumar@bombaysoftwares.com>
2024-08-07 00:44:46 +0530

    feat: projects[wip]

--- src/constants/index.js
+++ src/constants/index.js
@@ -94,10 +94,10 @@ const technologies = [
   //   name: "MongoDB",
   //   icon: mongodb,
   // },
-  // {
-  //   name: "Three JS",
-  //   icon: threejs,
-  // },
+  {
+    name: "Three JS",
+    icon: threejs,
+  },
   // {
   //   name: "git",
   //   icon: git,
