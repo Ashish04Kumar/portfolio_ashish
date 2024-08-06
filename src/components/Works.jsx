325fd17d0e99cf4cb5d1bcd0c6c9d660d0957921
bs-ashish-kumar <ashish.kumar@bombaysoftwares.com>
2024-08-06 23:32:05 +0530

    feat: projects

--- src/components/Works.jsx
+++ src/components/Works.jsx
@@ -2,7 +2,7 @@ import { motion } from 'framer-motion'
 import React from 'react'
 import { Tilt } from 'react-tilt'
 import { styles } from '../styles'
-import { github } from '../assets'
+// import { github } from '../assets'
 import { SectionWrapper } from '../hoc'
 import { projects } from '../constants'
 import { fadeIn , textVariant} from '../utils/motion'
@@ -10,8 +10,13 @@ import { fadeIn , textVariant} from '../utils/motion'
 
 function Works() {
   return (
-    <div>Works</div>
+    <>
+    <motion.div variants={textVariant()}>
+     <p className={styles.sectionSubText}>My Projects</p>
+      <h2 className={styles.sectionHeadText}>Projects.</h2>
+   </motion.div>
+    </>
   )
 }
 
-export default Works
+export default SectionWrapper(Works, "")
