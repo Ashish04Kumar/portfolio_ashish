16272bdad204dab56149e05e9c39755e1bdf8c31
bs-ashish-kumar <ashish.kumar@bombaysoftwares.com>
2024-08-07 00:51:11 +0530

    feat: projects[wip]

--- src/assets/index.js
+++ src/assets/index.js
@@ -22,7 +22,9 @@ import redux from "./tech/redux.png";
 import tailwind from "./tech/tailwind.png";
 import typescript from "./tech/typescript.png";
 // import threejs from "./tech/threejs.svg";
-import threejs from "./tech/nextjs.png";
+// import threejs from "./tech/threejs.svg";
+import threejs from "./tech/next_js.png";
+
 
 import bootstrap from "./tech/bootstrap.png"
 
